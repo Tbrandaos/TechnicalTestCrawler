34bad177da941b649cd2935dd47527c7c654cc32
Thiago Brandão <thiago_brandaos@outlook.com>
2023-06-25 01:59:02 -0300

    Add entities and context

---
+++ TechnicalTestCrawler.Infra/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechnicalTestCrawler.Infra.Entities;
+
+namespace TechnicalTestCrawler.Infra.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TechnicalTestCrawler.Infra/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace TechnicalTestCrawler.Infra.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+    }
+}
